eb7a71e00e538a2171e40b1e4f1de36f77569755
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-03-27 14:19:01 +0800

    Update test_oder_calendor.py

--- test_oder_calendor.py
+++ test_oder_calendor.py
@@ -8,7 +8,7 @@ def main():
     selected_date = st.date_input("選擇日期", datetime.today())
 
     # 選擇時間
-    #  selected_time = st.time_input("選擇時間", datetime.now().time())
+    # selected_time = st.time_input("選擇時間", datetime.now().time())
 
     # 選擇服務或項目
     service_options = ["中餐", "晚餐"]
@@ -17,7 +17,7 @@ def main():
     # 顯示訂單摘要
     st.subheader("訂單摘要")
     st.write(f"日期: {selected_date}")
-    st.write(f"時間: {selected_time}")
+    # st.write(f"時間: {selected_time}")
     st.write(f"服務或項目: {selected_service}")
 
     # 如果選擇了日期，顯示訂位按鈕
