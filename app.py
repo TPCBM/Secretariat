f87d1b04ec96706bee42d31f2c1b8806837501b4
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-04-03 13:37:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,13 @@
 import streamlit as st
 import pandas as pd
 from calendar_display import main as display_calendar
-from reservation_form import reservation_form, reservation_data
+from reservation_form import reservation_form
 
 def main():
     st.title("勵進餐廳預定")
+
+    # 创建一个全局变量用于保存预订信息
+    reservation_data = pd.DataFrame(columns=['Date', 'Period', 'Name', 'Phone'])
 
     option = st.sidebar.selectbox(
         '頁面選單',
@@ -17,7 +20,8 @@ def main():
         # 将预订信息传递给日历显示页面
         display_calendar(reservation_data)
     elif option == '填寫預定表單':
-        reservation_form()
+        # 将 reservation_data 作为参数传递给填写表单页面
+        reservation_form(reservation_data)
 
 if __name__ == "__main__":
     main()
