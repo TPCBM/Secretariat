b9e672c48dc748257b5564d722e0341fc42f3a23
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-04-03 13:39:56 +0800

    Update reservation_form.py

--- reservation_form.py
+++ reservation_form.py
@@ -1,10 +1,8 @@
-# reservation_form.py
-
 import streamlit as st
 import pandas as pd
 from datetime import datetime
 
-def reservation_form(reservation_data):  # 将 reservation_data 作为参数传入函数中
+def reservation_form(reservation_data):
     st.title("請填寫預定表單")
 
     date = st.date_input("訂位日期")
@@ -17,9 +15,9 @@ def reservation_form(reservation_data):  # 将 reservation_data 作为参数传�
             st.error("姓名和電話為必填！")
         else:
             # 将预订信息添加到全局变量中
-            global reservation_data
-            # 创建一个新的 DataFrame 包含新的预订数据，并与之前的数据合并
             new_reservation = pd.DataFrame({'Date': [date], 'Period': [period], 'Name': [name], 'Phone': [phone]})
             reservation_data = pd.concat([reservation_data, new_reservation], ignore_index=True)
 
             st.success("預定成功！")
+
+            return reservation_data  # 返回更新后的 reservation_data
