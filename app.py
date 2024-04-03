b672b0099b8a3532d92a4265ea8e6599316720e3
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-04-03 14:48:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ from reservation_form import reservation_form
 def main():
     st.title("勵進餐廳預定")
 
-    # 创建一个全局变量用于保存预订信息
-    reservation_data = pd.DataFrame(columns=['Date', 'Period', 'Name', 'Phone'])
+    # 使用st.session_state来存储和共享变量
+    if 'reservation_data' not in st.session_state:
+        st.session_state.reservation_data = pd.DataFrame(columns=['Date', 'Period', 'Name', 'Phone'])
 
     option = st.sidebar.selectbox(
         '頁面選單',
@@ -16,13 +17,10 @@ def main():
 
     if option == '空位查詢日曆':
         # 将预订信息传递给日历显示页面
-        display_calendar(reservation_data)
+        display_calendar(st.session_state.reservation_data)
     elif option == '填寫預定表單':
         # 调用 reservation_form 函数并将 reservation_data 作为参数传递给它
-        reservation_data = reservation_form(reservation_data)
-
-        # 更新日历显示
-        display_calendar(reservation_data)
+        st.session_state.reservation_data = reservation_form(st.session_state.reservation_data)
 
 if __name__ == "__main__":
     main()
