2c3b1be2ad126952d509793adb08a471c357ff4f
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-08-19 14:07:37 +0800

    Update dashboard2.py

--- dashboard/dashboard2.py
+++ dashboard/dashboard2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import numpy as np # 引入NumPy
-from matplotlib.pyplot as plt # 引入matplotlib的函數
+import matplotlib.pyplot as plt
+import numpy as np
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from datetime import datetime, timedelta
