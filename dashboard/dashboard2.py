e10d47ff69c2323e99fc22fcfee0d760c77bcf93
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-08-20 15:59:45 +0800

    Update dashboard2.py

--- dashboard/dashboard2.py
+++ dashboard/dashboard2.py
@@ -53,7 +53,7 @@ st.markdown("""
 daily_events.name = '申請停放數量'
 
 # 设置背景图片
-background_image_url = 'static/images/background.png'  # 替换为实际图片路径
+background_image_url = 'dashboard/x3xWPF.2-0.png'  # 替换为实际图片路径
 
 # 生成背景图的 CSS
 background_css = f"""
