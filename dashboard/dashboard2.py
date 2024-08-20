5098d689b6a602d79c1447fd189018898951daec
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-08-20 14:14:44 +0800

    Update dashboard2.py

--- dashboard/dashboard2.py
+++ dashboard/dashboard2.py
@@ -27,8 +27,8 @@ def get_calendar_events(calendar_id, start_time, end_time):
 
 # 設置日期範圍
 today = datetime.utcnow().date()
-start = datetime(today.year, today.month, 1)
-end = (start + timedelta(days=32)).replace(day=1)
+start = today
+end = today + timedelta(days=1)
 
 # 獲取事件
 calendar_id = 'sd2721idgjjildevu45dc6t4ek@group.calendar.google.com'  # 這裡可以替換為你的日曆 ID
