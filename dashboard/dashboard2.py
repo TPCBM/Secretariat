a9b3bf345fe60ca85e7d8a3ab1f47787b9e1915a
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-08-20 16:21:48 +0800

    Update dashboard2.py

--- dashboard/dashboard2.py
+++ dashboard/dashboard2.py
@@ -60,7 +60,9 @@ background_css = f"""
 <style>
 .report-container {{
     background: url('{background_image_url}');
-    background-size: contain;
+    background-size: contain;  /* 确保图片缩小显示 */
+    background-position: left top;  /* 图片靠左上角对齐 */
+    background-repeat: no-repeat;  /* 防止图片重复 */
     padding: 20px;
     border-radius: 10px;
     color: white;
