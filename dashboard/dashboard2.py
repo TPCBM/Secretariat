1d503754389d2daf174a4d80b913ab3ff092af9e
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-08-21 10:55:20 +0800

    Update dashboard2.py

--- dashboard/dashboard2.py
+++ dashboard/dashboard2.py
@@ -48,7 +48,7 @@ def get_latest_sheet_data(spreadsheet_id, sheet_name):
 
 # *設置 Google Sheets 資訊
 SPREADSHEET_ID = '1XiBEOWus9hnXzAMOX5dss-nheYKFEEIzeUzpcYXzmMM'  # 替換為你的 Google Sheets ID
-SHEET_NAME = 'Sheet1'  # 替換為你的工作表名稱
+SHEET_NAME = '表單回應 1'  # 替換為你的工作表名稱
 
 # *顯示 Google Sheets 中的最新一筆資料
 latest_data = get_latest_sheet_data(SPREADSHEET_ID, SHEET_NAME)
