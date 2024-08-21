c3fe74e3651e46f9b2073ff05aed1da333807477
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-08-21 16:35:00 +0800

    Update dashboard2.py

--- dashboard/dashboard2.py
+++ dashboard/dashboard2.py
@@ -74,7 +74,7 @@ st.markdown("""
         color: white;
     }
     .section1 h2 {
-        font-size: 24px;  /* 調整字體大小 */
+        font-size: 33px;  /* 調整字體大小 */
     }
     .section2 {
         background-color: #f0f0f0;  /* 淺灰色背景 */
