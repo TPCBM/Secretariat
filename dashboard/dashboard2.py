d1960307c72232347f544628eff264b07aeedc3d
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-08-21 11:28:22 +0800

    Update dashboard2.py

--- dashboard/dashboard2.py
+++ dashboard/dashboard2.py
@@ -111,5 +111,16 @@ with col1:
 
 # 在第二列顯示「總處大小事(最新一筆)」
 with col2:
+    # 拆分 latest_data 的列
+    columns_part1 = latest_data.iloc[:, :4]  # 取前4列
+    columns_part2 = latest_data.iloc[:, 4:]  # 取第5列及後面的列
+
+    # 顯示「總處大小事(最新一筆)」
     st.subheader('總處大小事(最新一筆)')
-    st.write(latest_data.style.applymap(lambda x: 'background-color: lightblue', subset=latest_data.columns))
+
+    # 第一部分表格顯示前4列
+    st.write(columns_part1.style.applymap(lambda x: 'background-color: lightblue', subset=columns_part1.columns))
+
+    # 顯示第二部分表格，換行顯示剩餘列
+    st.write(columns_part2.style.applymap(lambda x: 'background-color: lightblue', subset=columns_part2.columns))
+
