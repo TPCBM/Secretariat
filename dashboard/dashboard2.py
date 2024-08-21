fde0ac9d611f4688bfcd358bde73bf62203ead0d
TPCBM <111564218+TPCBM@users.noreply.github.com>
2024-08-21 11:34:15 +0800

    Update dashboard2.py

--- dashboard/dashboard2.py
+++ dashboard/dashboard2.py
@@ -112,15 +112,18 @@ with col1:
 # 在第二列顯示「總處大小事(最新一筆)」
 with col2:
     # 拆分 latest_data 的列
-    columns_part1 = latest_data.iloc[:, :4].reset_index(drop=True)  # 取前4列並重置索引
-    columns_part2 = latest_data.iloc[:, 4:].reset_index(drop=True)  # 取第5列及後面的列並重置索引
+    columns_part1 = latest_data.iloc[:, :4]  # 取前4列
+    columns_part2 = latest_data.iloc[:, 4:]  # 取第5列及後面的列
 
     # 顯示「總處大小事(最新一筆)」
     st.subheader('總處大小事(最新一筆)')
 
-    # 第一部分表格顯示前4列
-    st.write(columns_part1.style.applymap(lambda x: 'background-color: lightblue', subset=columns_part1.columns))
+    # 第一部分表格顯示前4列，隱藏索引
+    st.write(columns_part1.style.applymap(lambda x: 'background-color: lightblue', subset=columns_part1.columns)
+             .hide(axis='index'))
 
-    # 顯示第二部分表格，換行顯示剩餘列
-    st.write(columns_part2.style.applymap(lambda x: 'background-color: lightblue', subset=columns_part2.columns))
+    # 顯示第二部分表格，換行顯示剩餘列，隱藏索引
+    st.write(columns_part2.style.applymap(lambda x: 'background-color: lightblue', subset=columns_part2.columns)
+             .hide(axis='index'))
+
 
